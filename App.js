13138414af56438bf37e684fbc64cfde6e683748
Imamsha <imamshas125@gmail.com>
2024-05-05 20:57:23 +0530

    setup

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-white" >
-       <Text className='text-center mt-3 text-2xl font-bold text-red-500'>
-        Welcome
+       <Text className='text-center mt-3 text-2xl underline font-bold text-red-500'>
+        Welcome 
       </Text>
       <StatusBar style="auto" />
     </View>
